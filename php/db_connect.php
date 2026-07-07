09d0faaf7b3ebe945dc9c165dc1e1ffc35b4e63b
CHamod-MAdusanka <madusankac965@gmail.com>
2026-07-07 13:39:21 +0530

    update db_conntent.php

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,18 +1,16 @@
 <?php
-// සර්වර් එකේ විස්තර
-$servername = "localhost";
-$username = "root";       // XAMPP එකේ default username එක
-$password = "";           // XAMPP එකේ default password එක හිස්
-$dbname = "smart_e_library"; // අපි හදපු ඩේටාබේස් එකේ නම
 
-// දත්ත ගබඩාවට සම්බන්ධ වීම
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "smart_e_library_new";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// සම්බන්ධතාවය හරිදැයි පරීක්ෂා කිරීම
 if ($conn->connect_error) {
     die("Database Connection Failed: " . $conn->connect_error);
-} else {
-    // ටෙස්ට් කරන්න විතරක් මේක දාමු
-    // echo "Database Successfully Connected....!"; 
 }
+
+$conn->set_charset("utf8mb4");
+
 ?>
